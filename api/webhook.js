a0619c9308a3e73f4be24b94ba3ba279a7012915
1031wataru <notify.alert.bot@gmail.com>
2025-04-23 11:05:20 +0900

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -2,7 +2,6 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     const events = req.body.events;
 
-    // イベントごとに処理（複数イベントに対応）
     const replies = events.map(async (event) => {
       const replyToken = event.replyToken;
       const userMessage = event.message?.text;
@@ -12,7 +11,7 @@ export default async function handler(req, res) {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer <アクセストークンをここに入れる>`, // 次のステップで説明！
+            'Authorization': 'Bearer VBEW6dMjWIUrWxBXIi/3V4JXYzZ8RD/5Q7SvvNWR875esT2mPEs39flUTQcfFbSwAVcRPANUcFdUPia0FEzzWgCVNZg+NhQdDzMybM0vc6s8SIh7g6oEKW4jJqBbvbiqOPi2jNpWAB+EzmrI+5LOhAdB04t89/1O/w1cDnyilFU=',
           },
           body: JSON.stringify({
             replyToken: replyToken,
