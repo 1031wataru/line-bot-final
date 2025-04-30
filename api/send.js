7346ec6b92dbf3dc48a4c33ad8edf473a6d7d004
1031wataru <notify.alert.bot@gmail.com>
2025-04-30 16:26:56 +0900

    Update send.js

--- api/send.js
+++ api/send.js
@@ -5,17 +5,17 @@ export default async function handler(req, res) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer VBEW6dMjWIUrWxBXIi/3V4JXYzZ8RD/5Q7SvvNWR875esT2mPEs39flUTQcfFbSwAVcRPANUcFdUPia0FEzzWgCVNZg+NhQdDzMybM0vc6s8SIh7g6oEKW4jJqBbvbiqOPi2jNpWAB+EzmrI+5LOhAdB04t89/1O/w1cDnyilFU=',
+      'Authorization': 'Bearer VBEW6dMjWIUrWxBXIi/3V4JXYzZ8RD/5Q7SvvNWR875esT2mPEs39f1UTQcfFbSwAVcRPANUcFdUPia0FEzzWgCVNZg+NhQdDzMybM0vc6s8SIh7g6oEKW4jJqBbvbiqOPi2jNpWAB+EzmrI+5LOhAdB04t89/1O/w1cDnyilFU='
     },
     body: JSON.stringify({
-      to: '君のユーザーID', // ← 次で説明！
+      to: 'Uf783157850ea9d9e24d4a9accdxxxxxxxx',
       messages: [
         {
           type: 'text',
-          text: message,
-        },
-      ],
-    }),
+          text: message
+        }
+      ]
+    })
   });
 
   res.status(200).send('通知を送信しました！');
